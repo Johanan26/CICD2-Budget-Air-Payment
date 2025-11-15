68d8b34a3444a5f197ce8b3b64c88b85d6926fc5
Johanan26 <120645993+Johanan26@users.noreply.github.com>
2025-11-15 22:18:39 +0000

    Payment Initalization

---
+++ app/Paymentschemas.py
@@ -0,0 +1,7 @@
+# app/schemas.py
+from typing import Annotated
+from annotated_types import Ge, Le
+from pydantic import BaseModel, EmailStr, StringConstraints
+from pydantic import BaseModel, EmailStr, Field
+from uuid import uuid4
+
