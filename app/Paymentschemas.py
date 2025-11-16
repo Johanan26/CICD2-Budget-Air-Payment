ddb82e8bb82bd421893e35705065b2411b6a7afd
Johanan26 <120645993+Johanan26@users.noreply.github.com>
2025-11-16 21:50:05 +0000

    made initial  paymentschema.py

--- app/Paymentschemas.py
+++ app/Paymentschemas.py
@@ -1,7 +1,58 @@
 # app/schemas.py
-from typing import Annotated
+from typing import Annotated, Optional, Dict, Any
+from datetime import datetime
+from decimal import Decimal
+from enum import Enum
 from annotated_types import Ge, Le
-from pydantic import BaseModel, EmailStr, StringConstraints
-from pydantic import BaseModel, EmailStr, Field
-from uuid import uuid4
+from pydantic import BaseModel, EmailStr, StringConstraints, Field
 
+#Enum
+class PaymentStatus(str, Enum):
+    PENDING = "pending"
+    AUTHORIZED = "authorized"
+    SUCCESS = "success"
+    FAILED = "failed"
+    CANCELED = "cancled"
+    REFUNDED = "refunded"
+
+class PaymentProvider(str, Enum):
+    STRIPE = "stripe"
+    PAYPAL = "paypal"
+    ADYEN = "adyen"
+    INTERNAL = "internal"
+
+class Currency(str, Enum):
+    EUR = "EUR"
+    USD = "USD"
+    GBP = "GBP"
+
+class PaymentBase(BaseModel):
+    amount: Decimal = Field(..., gt=0, description="Amount to charge in major units") 
+    current: Currency = Field(..., description="Currency of the payment")
+    description: Optional[str] = Field(None, description="Human readable description")
+    metadata: Dict[str, Any] = Field(default_factory=dict, description="Random data")
+    user_id: Optional[str] = Field(None, description="ID of the user making the payment")
+    order_id: Optional[str] = Field(None, description="ID of the related order in your system")
+    provider: PaymentProvider = Field(default=PaymentProvider.INTERNAL)
+    provider_payment_id: Optional[str] = Field(None, description="ID of payment in the PSP")
+
+#Input models
+class PaymentUpdateStatus(BaseModel):
+    status: PaymentStatus
+    failure_reason: Optional[str] = Field(None, description="Reason for failure / cancel if application(from provider or internal)")
+
+class PaymentRefundCreate(BaseModel):
+    payment_id: str
+    amount: Optional[Decimal] = Field(None, gt=0, description="Ifomitted, refund full remaining amount")
+    reason: Optional[str] = None
+
+#Output model
+class Payment(PaymentBase):
+    id:str
+    status: PaymentStatus = PaymentStatus.PENDING
+    failure_reason: Optional[str] = None
+    created_at: datetime
+    update_at: datetime
+
+    class Config:
+        from_attributes = True
